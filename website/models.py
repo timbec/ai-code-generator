a310e1da538711bb2a24bb0649eaa3b9a5708cc6
Tim Beckett <timbeckett@Tims-Mac-mini.home>
2023-04-27 11:24:54 -0400

    basic functionality set up.

--- website/models.py
+++ website/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Code(models.Model):
+    user = models.ForeignKey(User, related_name="code",
+                             on_delete=models.DO_NOTHING)
+    question = models.TextField(max_length=5000)
+    code_answer = models.TextField(max_length=5000)
+    language = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.question
